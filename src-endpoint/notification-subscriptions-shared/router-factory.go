8cf9b0288f245d80252fb1f66bf3f34ae9b577db
ccobascosha <ccobascosha@users.noreply.github.com>
2025-11-27 12:03:13 +0000

    Add router-factory.go, router-test.less

---
+++ src-endpoint/notification-subscriptions-shared/router-factory.go
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/smartmeter_z/src-endpoint/notification-subscriptions-shared/router-factory.go
+// Generated at: 2025-11-27T12:25:22.139Z
+
